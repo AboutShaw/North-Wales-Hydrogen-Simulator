679eca6f85161adcc629b563893bf9ef548997d1
AboutShaw <shawsi1988@gmail.com>
2024-10-04 15:53:56 +0100

    deionised water programmed in to prodcue when power is available and deduct power from pool. Also tracks how much diWater is available

--- app.js
+++ app.js
@@ -1,2 +1,37 @@
-import { timeSimulator } from "./functions/timePowerSim.js";
+let dayNumber = 1;
+let hours = 0;
+let minutes = 0;
+let formattedTime;
+let powerAvailable = 0;
+let diWaterAvailable = 0;
+const diWaterRate = 2;
+const diPowerRate = -3;
+function timeSimulator() {
+    setInterval(() => {
+        minutes++;
+        if (minutes === 60) {
+            minutes = 0;
+            hours++;
+        }
+        if (hours === 24) {
+            hours = 0;
+            dayNumber++;
+        }
+        if (dayNumber <= 365) {
+            formattedTime = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
+            // Calculate power output based on the hour of the day
+            if (hours >= 1 && hours <= 5) {
+                powerAvailable = 105;
+                if (powerAvailable > 0) {
+                    powerAvailable = powerAvailable + diPowerRate;
+                    diWaterAvailable = diWaterAvailable + diWaterRate;
+                }
+            }
+        }
+        else {
+            clearInterval(this);
+        }
+        console.log(`Day ${dayNumber}: ${formattedTime}, Power avialable: ${powerAvailable} MW, DI Water available: ${diWaterAvailable}`);
+    }, 250);
+}
 timeSimulator();
--- app.ts
+++ app.ts
@@ -1,3 +1,48 @@
-import { timeSimulator } from "./functions/timePowerSim.js";
-  
-  timeSimulator();
+let dayNumber = 1;
+    let hours = 0;
+    let minutes = 0;
+    let formattedTime;
+    let powerAvailable = 0; // MW per hour
+    let diWaterAvailable = 0; // m3
+    const diWaterRate = 2; // m3 per minute
+    const diPowerRate = -3; // MW per hour
+
+function timeSimulator() {
+
+    setInterval(() => {
+        minutes++;
+        if (minutes === 60) {
+            minutes = 0;
+            hours++;
+        }
+        if (hours === 24) {
+            hours = 0;
+            dayNumber++;
+        }
+        if (dayNumber <= 365) {
+            formattedTime = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
+            // Calculate power output based on the hour of the day
+           
+            if (hours >= 1 && hours <= 5) {
+                powerAvailable = 105;
+                if (powerAvailable > 0) {
+                    powerAvailable = powerAvailable + diPowerRate;
+                    diWaterAvailable = diWaterAvailable + diWaterRate;
+                }
+                
+            }
+            
+        }
+        else {
+            clearInterval(this);
+        }
+
+        console.log(`Day ${dayNumber}: ${formattedTime}, Power avialable: ${powerAvailable} MW, DI Water available: ${diWaterAvailable}`);
+
+
+    }, 250);
+}
+
+
+
+timeSimulator();

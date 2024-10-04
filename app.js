79e3b8d4946ec4964f0fb92c71c7c3737d5feddf
AboutShaw <shawsi1988@gmail.com>
2024-10-04 21:36:40 +0100

    end of day commit code working

--- app.js
+++ app.js
@@ -12,21 +12,22 @@ const diWaterRate = 310; // kg per minute
 const diPowerRate = -0.0372; // MW per hour
 let hydrogenproduced = 0; // kgs
 let deesidePipeInLine1 = 0; // kgs of H2
-const deesidePipeInLine1Max = 50; // kgs of H2
+const deesidePipeInLine1Max = 87.316; // kgs of H2
 let deesideStorage1 = 0; // kgs of H2
 const deesideStorage1Max = 2000; // kgs of H2
 let deesidePipeInLine2 = 0; // kgs of H2
-const deesidePipeInLine2Max = 50; // kgs of H2
+const deesidePipeInLine2Max = 28.767; // kgs of H2
 let deesideStorage2 = 0; // kgs of H2
 const deesideStorage2Max = 2000; // kgs of H2
 let deesidePipeInLine3 = 0; // kgs of H2
-const deesidePipeInLine3Max = 50; // kgs of H2
+const deesidePipeInLine3Max = 25.051; // kgs of H2
 let deesideStorage3 = 0; // kgs of H2
 const deesideStorage3Max = 2000; // kgs of H2
 let deesidePipeInLine4 = 0; // kgs of H2
-const deesidePipeInLine4Max = 50; // kgs of H2
+const deesidePipeInLine4Max = 22.809; // kgs of H2
 let deesideHydrogenHub = 0; // kgs of H2
 let deesideHydrogenHubMax = 2000; // kgs of H2
+const systemH2Max = 8163.943; // kg of H2
 let electroProductionMax = 31.944; // kgs per minute @ 100%
 const electroPowerMax = -100; //MW per hour
 const electroDIConsumption = -308.333; // kgs per min
@@ -46,6 +47,11 @@ function timeSimulator() {
             // Calculate power output based on the hour of the day
             if (hours >= 1 && hours <= 5) {
                 powerAvailable = powerMax;
+                if (powerAvailable > 0 && hydrogenproduced >= systemH2Max) {
+                    charge = (powerAvailable * chargeRatio) / 60;
+                    batteryStorage = batteryStorage + charge;
+                }
+                ;
                 if (powerAvailable > 0) {
                     powerAvailable = powerAvailable + diPowerRate + electroPowerMax;
                     charge = (powerAvailable * chargeRatio) / 60;
--- app.ts
+++ app.ts
@@ -15,21 +15,22 @@ const diPowerRate = -0.0372; // MW per hour
 
 let hydrogenproduced = 0 // kgs
 let deesidePipeInLine1 = 0; // kgs of H2
-const deesidePipeInLine1Max = 50; // kgs of H2
+const deesidePipeInLine1Max = 87.316; // kgs of H2
 let deesideStorage1 = 0; // kgs of H2
 const deesideStorage1Max = 2000; // kgs of H2
 let deesidePipeInLine2 = 0; // kgs of H2
-const deesidePipeInLine2Max = 50; // kgs of H2
+const deesidePipeInLine2Max = 28.767; // kgs of H2
 let deesideStorage2 = 0; // kgs of H2
 const deesideStorage2Max = 2000; // kgs of H2
 let deesidePipeInLine3 = 0; // kgs of H2
-const deesidePipeInLine3Max = 50; // kgs of H2
+const deesidePipeInLine3Max = 25.051; // kgs of H2
 let deesideStorage3 = 0; // kgs of H2
 const deesideStorage3Max = 2000; // kgs of H2
 let deesidePipeInLine4 = 0; // kgs of H2
-const deesidePipeInLine4Max = 50; // kgs of H2
+const deesidePipeInLine4Max = 22.809; // kgs of H2
 let deesideHydrogenHub = 0; // kgs of H2
 let deesideHydrogenHubMax = 2000; // kgs of H2
+const systemH2Max = 8163.943; // kg of H2
 
 let electroProductionMax = 31.944; // kgs per minute @ 100%
 const electroPowerMax = -100; //MW per hour
